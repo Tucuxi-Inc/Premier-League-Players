76aa6e8460f9e479649c971f48be2a4f9ed1e958
Kevin-Tucuxi <kevin@tucuxi.ai>
2025-01-19 23:27:49 -0800

    Initial Commit

---
+++ Premier League Players/Premier_League_PlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Premier_League_PlayersApp.swift
+//  Premier League Players
+//
+//  Created by Kevin Keller on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Premier_League_PlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Premier League PlayersTests/Premier_League_PlayersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Premier_League_PlayersTests.swift
+//  Premier League PlayersTests
+//
+//  Created by Kevin Keller on 1/19/25.
+//
+
+import Testing
+@testable import Premier_League_Players
+
+struct Premier_League_PlayersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
